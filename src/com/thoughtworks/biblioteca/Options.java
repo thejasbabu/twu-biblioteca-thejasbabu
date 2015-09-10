17934abf74e82cc5cd299c3bb5fab186041cda0e
thejasbabu <thejasb@thoughtworks.com>
2015-09-10 23:17:20 +0530

    Introducing interface Option for different options in main menu

---
+++ src/com/thoughtworks/biblioteca/Options.java
@@ -0,0 +1,8 @@
+// Option interface is a skeleton for all the options which will be in the menu of Biblioteca
+package com.thoughtworks.biblioteca;
+
+public interface Options {
+
+ public String execute();
+
+}
