9a5b257723ccaafe376b520ffde3391cc7bd9ebd
thejasbabu <thejasb@thoughtworks.com>
2015-09-14 21:05:34 +0530

    Added another spec for checout movie option

--- test/com/thoughtworks/biblioteca/CheckOutMovieOptionTest.java
+++ test/com/thoughtworks/biblioteca/CheckOutMovieOptionTest.java
@@ -22,4 +22,18 @@ public class CheckOutMovieOptionTest {
 
         assertEquals("Thank you! Enjoy your movie\n", checkOutMovieOption.execute());
     }
+
+    @Test
+    public void shouldReturnErrorMessageWhenCheckoutIsNotSuccessful() {
+        Display display = new Display(new PrintStream(System.out));
+        Input input = mock(Input.class);
+        Library library = mock(Library.class);
+        CheckOutMovieOption checkOutMovieOption = new CheckOutMovieOption(library, input, display);
+
+        when(input.read()).thenReturn("Interstellar");
+        when(library.checkOutMovie("Interstellar")).thenReturn(false);
+
+        assertEquals("This movie is not available\n", checkOutMovieOption.execute());
+
+    }
 }
