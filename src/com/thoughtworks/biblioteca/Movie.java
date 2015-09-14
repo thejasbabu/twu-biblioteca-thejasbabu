9472571b0740f5ef518d39eada5efba633e0eb63
thejasbabu <thejasb@thoughtworks.com>
2015-09-14 16:46:44 +0530

    Added movie class with specs

---
+++ src/com/thoughtworks/biblioteca/Movie.java
@@ -0,0 +1,23 @@
+// Movie class represents a movie which is present in the library.
+package com.thoughtworks.biblioteca;
+
+public class Movie {
+    private String name;
+    private String year;
+    private String director;
+    private String rating;
+
+    public Movie(String name, String year, String director, String rating) {
+        this.name = name;
+        this.year = year;
+        this.director = director;
+        this.rating = rating;
+    }
+
+    @Override
+    public String toString() {
+        String movieList;
+        movieList = String.format("%-40s%-40s%-40s", name, year, director, rating);
+        return movieList;
+    }
+}
---
+++ test/com/thoughtworks/biblioteca/MovieTest.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.biblioteca;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MovieTest {
+
+    @Test
+    public void shouldReturnBookInColumnarFashion() {
+        Movie movie = new Movie("The Good,The Bad and The Ugly", "1966", "Sergio Leone", "8.9");
+
+        String movieList = String.format("%-40s%-40s%-40s","The Good,The Bad and The Ugly", "1966", "Sergio Leone", "8.9");
+        assertEquals(movieList, movie.toString());
+    }
+}
