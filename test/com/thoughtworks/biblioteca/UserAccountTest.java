f8bfc714fc6acdacdc57a0d8c59f71ae9ce8801f
thejasbabu <thejasb@thoughtworks.com>
2015-09-15 16:56:48 +0530

    Added spec for authenticate method

--- test/com/thoughtworks/biblioteca/UserAccountTest.java
+++ test/com/thoughtworks/biblioteca/UserAccountTest.java
@@ -19,4 +19,30 @@ public class UserAccountTest {
 
         assertEquals(user, userAccount.authenticate("LIB-1234", "Meh123"));
     }
+
+    @Test
+    public void shouldReturnUserObjectWithLibrarianRoleWhenValidLibrarianIsLoggedIn() {
+        ArrayList<User> users = new ArrayList<User>();
+        users.add(new User("LIB-1234", "Meh123", User.Role.CUSTOMER));
+        users.add(new User("LIB-1235", "Meh1234", User.Role.LIBRARIAN));
+        users.add(new User("LIB-1236", "Meh1235", User.Role.INVALID));
+        UserAccount userAccount = new UserAccount(users);
+
+        User user = new User("LIB-1235", "Meh1234", User.Role.LIBRARIAN);
+
+        assertEquals(user, userAccount.authenticate("LIB-1235", "Meh1234"));
+    }
+
+    @Test
+    public void shouldReturnUserObejctWithInvalidRoleWhenSomeGuestISLoggedIn() {
+        ArrayList<User> users = new ArrayList<User>();
+        users.add(new User("LIB-1234", "Meh123", User.Role.CUSTOMER));
+        users.add(new User("LIB-1235", "Meh1234", User.Role.LIBRARIAN));
+        users.add(new User("LIB-1236", "Meh1235", User.Role.INVALID));
+        UserAccount userAccount = new UserAccount(users);
+
+        User user = new User("LIB-1236", "Meh1235", User.Role.INVALID);
+
+        assertEquals(user, userAccount.authenticate("LIB-1236", "Meh1235"));
+    }
 }
