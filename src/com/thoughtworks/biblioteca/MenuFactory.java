c9f8e27cfd9192cd68d9330f21a9d6262fc67e5f
thejasbabu <thejasb@thoughtworks.com>
2015-09-16 20:51:35 +0530

    Added specs to menufactory

--- src/com/thoughtworks/biblioteca/MenuFactory.java
+++ src/com/thoughtworks/biblioteca/MenuFactory.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 public class MenuFactory {
 
     public Menu getMenu(User.Role role) {
-        if(role.equals(User.Role.CUSTOMER)) {
-            ArrayList<String> menuList = new ArrayList<String>();
+        ArrayList<String> menuList = new ArrayList<String>();
+        if(role.equals(User.Role.INVALID)) {
             menuList.add("1. List Book");
             menuList.add("4. Exit");
             menuList.add("5. List Movie");
@@ -15,7 +15,30 @@ public class MenuFactory {
             Menu menu = new Menu(menuList);
             return menu;
         }
+        else if(role.equals(User.Role.CUSTOMER)) {
+            menuList.add("1. List Book");
+            menuList.add("2. Checkout Book");
+            menuList.add("3. Return Book");
+            menuList.add("4. Exit");
+            menuList.add("5. List Movie");
+            menuList.add("6. Checkout Movie");
+            menuList.add("8. Logout");
+            menuList.add("10. User Details");
+            Menu menu = new Menu(menuList);
+            return menu;
+        }
         else
-            return new Menu(new ArrayList<String>());
+        {
+            menuList.add("1. List Book");
+            menuList.add("2. Checkout Book");
+            menuList.add("3. Return Book");
+            menuList.add("4. Exit");
+            menuList.add("5. List Movie");
+            menuList.add("6. Checkout Movie");
+            menuList.add("8. Logout");
+            menuList.add("9. Book details");
+            Menu menu = new Menu(menuList);
+            return menu;
+        }
     }
 }
--- test/com/thoughtworks/biblioteca/MenuFactoryTest.java
+++ test/com/thoughtworks/biblioteca/MenuFactoryTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.*;
 public class MenuFactoryTest {
 
     @Test
-    public void shouldReturnCustomerMenuItemWhenUserISCustomer() {
+    public void shouldReturnInvalidMenuItemWhenUserIsInvalid() {
         MenuFactory menuFactory = new MenuFactory();
         ArrayList<String> menuList = new ArrayList<String>();
         menuList.add("1. List Book");
@@ -19,7 +19,41 @@ public class MenuFactoryTest {
         menuList.add("7. Login");
         Menu menu = new Menu(menuList);
 
+        assertEquals(menu.toString(), menuFactory.getMenu(User.Role.INVALID).toString());
+    }
+
+    @Test
+    public void shouldReturnCustomerMenuWhenUserIsCustomer() {
+        MenuFactory menuFactory = new MenuFactory();
+        ArrayList<String> menuList = new ArrayList<String>();
+        menuList.add("1. List Book");
+        menuList.add("2. Checkout Book");
+        menuList.add("3. Return Book");
+        menuList.add("4. Exit");
+        menuList.add("5. List Movie");
+        menuList.add("6. Checkout Movie");
+        menuList.add("8. Logout");
+        menuList.add("10. User Details");
+        Menu menu = new Menu(menuList);
+
         assertEquals(menu.toString(), menuFactory.getMenu(User.Role.CUSTOMER).toString());
     }
-    
+
+    @Test
+    public void shouldReturnLibrarianMenuWhenUserIsLibrarian() {
+        MenuFactory menuFactory = new MenuFactory();
+        ArrayList<String> menuList = new ArrayList<String>();
+        menuList.add("1. List Book");
+        menuList.add("2. Checkout Book");
+        menuList.add("3. Return Book");
+        menuList.add("4. Exit");
+        menuList.add("5. List Movie");
+        menuList.add("6. Checkout Movie");
+        menuList.add("8. Logout");
+        menuList.add("9. Book details");
+        Menu menu = new Menu(menuList);
+
+        assertEquals(menu.toString(), menuFactory.getMenu(User.Role.LIBRARIAN).toString());
+
+    }
 }
